adb32e14eb82b37fa7844651e2ff4cacc5973c87
GustavoMSantoss <moreira.santos.gus@gmail.com>
2025-10-28 19:51:55 -0300

    (25/10) Finalização da reorganização das rotas do front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,65 +1,34 @@
-import React from 'react'
+
 import { Routes, Route } from 'react-router-dom'
+
+
 import AuthGuard from './AuthGuard'
 
-import Homepage from '../pages/Homepage'
 
-import CarForm from '../pages/car/CarForm'
-import CarList from '../pages/car/CarList'
+import { routes, UserLevel } from './routes'
 
-import CustomerForm from '../pages/customer/CustomerForm'
-import CustomerList from '../pages/customer/CustomerList'
-
-import UserList from '../pages/user/UserList'
-import UserForm from '../pages/user/UserForm'
-
-import Login from '../pages/Login'
 
 export default function AppRoutes() {
-  return <Routes>
-    <Route path="/" element={ <Homepage /> } />
-
-    <Route path="/login" element={ <Login /> } />
-        <Route path="/cars" element={ <AuthGuard> <CarList /> </AuthGuard> } />
-    <Route path="/cars/new" element={ <AuthGuard> <CarForm /> </AuthGuard> } />
-    <Route path="/cars/:id" element={ <AuthGuard> <CarForm /> </AuthGuard> } />
-
-    <Route path="/customers" element={ 
-      <AuthGuard> <CustomerList /> </AuthGuard> 
-    } />
-
-    <Route path="/customers/new" element={ 
-      <AuthGuard> <CustomerForm /> </AuthGuard>
-    } />
-    <Route path="/customers/:id" element={ 
-      <AuthGuard> <CustomerForm /> </AuthGuard>  
-    } />
-
-    <Route path="/users" element={ 
-      <AuthGuard adminOnly={true}> <UserList /> </AuthGuard> } 
-    />
-    <Route path="/users/new" element={ 
-      <AuthGuard adminOnly={true}> <UserForm /> </AuthGuard> } 
-    />
-    <Route path="/users/:id" element={ 
-      <AuthGuard adminOnly={true}> <UserForm /> </AuthGuard> } 
-    />
-
-
-    <Route path="/cars" element={ <CarList /> } />
-    <Route path="/cars/new" element={ <CarForm /> } />
-    <Route path="/cars/:id" element={ <CarForm /> } />
-
-    <Route path="/customers" element={ 
-      <CustomerList /> 
-    } />
-    
-    <Route path="/customers/new" element={ <CustomerForm />} />
-    <Route path="/customers/:id" element={ <CustomerForm />  } />
-
-    <Route path="/users" element={ <UserList /> } />
-    <Route path="/users/new" element={ <UserForm /> } />
-    <Route path="/users/:id" element={ <UserForm /> } />
-    
-  </Routes>
+ return (
+   <Routes>
+     {
+       routes.map(route => {
+         let element
+         if(route.userLevel > UserLevel.ANY) {
+           element = <AuthGuard userLevel={route.userLevel}>
+             {route.element}
+           </AuthGuard>
+         }
+         else element = route.element
+        
+         return <Route
+           key={route.route}
+           path={route.route}
+           element={element}
+         />
+       })
+     }
+   </Routes>
+ )
 }
+
