aa5a81790fbbbeff577686c8a11b259443dc1845
akcio23 <dev.akcio@gmail.com>
2025-11-04 19:07:52 -0300

    (01/11) Término do ataque de força bruta e início da implementação de validação de dados de usuário

--- back-end/src/controllers/customers.js
+++ back-end/src/controllers/customers.js
@@ -1,9 +1,22 @@
 import prisma from '../database/client.js'
+import Customer from '../models/Customer.js'
+import { ZodError } from 'zod'
 
 const controller = {}     // Objeto vazio
 
 controller.create = async function(req, res) {
   try {
+
+     // Sempre que houver um campo que represente uma data,
+   // precisamos garantir sua conversão para o tipo Date
+   // antes de passá-lo ao Zod para validação
+   if(req.body.birth_date) req.body.birth_date = new Date(req.body.birth_date)
+
+
+   // Invoca a validação do modelo do Zod para os dados que
+   // vieram em req.body
+   Customer.parse(req.body)
+
 
     await prisma.customer.create({ data: req.body })
 
@@ -12,6 +25,11 @@ controller.create = async function(req, res) {
   }
   catch(error) {
     console.error(error)
+
+     // Se for erro de validação do Zod, retorna
+   // HTTP 422: Unprocessable Entity
+   if(error instanceof ZodError) res.status(422).send(error.issues)
+
 
     // HTTP 500: Internal Server Error
     res.status(500).end()
@@ -64,6 +82,17 @@ controller.retrieveOne = async function(req, res) {
 
 controller.update = async function(req, res) {
   try {
+
+    // Sempre que houver um campo que represente uma data,
+   // precisamos garantir sua conversão para o tipo Date
+   // antes de passá-lo ao Zod para validação
+   if(req.body.birth_date) req.body.birth_date = new Date(req.body.birth_date)
+
+
+   // Invoca a validação do modelo do Zod para os dados que
+   // vieram em req.body
+   Customer.parse(req.body)
+
     const result = await prisma.customer.update({
       where: { id: Number(req.params.id) },
       data: req.body
@@ -75,10 +104,20 @@ controller.update = async function(req, res) {
     else res.status(404).end()
   }
   catch(error) {
-    console.error(error)
+ console.error(error)
 
-    // HTTP 500: Internal Server Error
-    res.status(500).end()
+
+   // Não encontrou e não atualizou ~> HTTP 404: Not Found
+   if(error?.code === 'P2025') res.status(404).end()
+
+
+   // Erro do Zod ~> HTTP 422: Unprocessable Entity
+   else if(error instanceof ZodError) res.status(422).send(error.issues)
+
+
+   // Outros erros ~> HTTP 500: Internal Server Error
+   else res.status(500).end()
+
   }
 }
 
