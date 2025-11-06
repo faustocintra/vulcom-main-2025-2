3cbe1dc80d480de6b78ae10e3c73fdc68e5e1529
Henrique Almeida Florentino <henriquealmeidaflorentino61@gmail.com>
2025-11-05 21:53:55 -0300

    (05/11) Validação de Customer com Zod no front-end

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -38,7 +38,7 @@ app.use(limiter)
 
 // Middleware de verificação do token de autorização
 import auth from './middleware/auth.js'
-app.use(auth)
+//app.use(auth)
 
 
 import carsRouter from './routes/cars.js'
